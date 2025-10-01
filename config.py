63b937da2daf6167aa38b38f260140fc03543751
SundaresanAI <ss@azentyk.ai>
2025-10-01 21:27:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ load_dotenv()
 
 TWILIO_ACCOUNT_SID = "ACaaaa83e9255e4d10cc5ca3168a510b07"
 TWILIO_AUTH_TOKEN = "11b64e53dc4b1b3bcdcb165fae510792"
-PUBLIC_URL = "https://9e1f23fea783.ngrok-free.app" 
+PUBLIC_URL = "https://azentyk-care-connect-e6cqapcvdhfsdpdv.centralus-01.azurewebsites.net/" 
 TWILIO_CALLER_ID = "+16093090331"  # Your Twilio number
 VOICE_VOICE = "Google.en-IN-Chirp3-HD-Puck"
